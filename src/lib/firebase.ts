4db93e59ae7e6d592f55bc21e9d61f7f0bf748d4
admir agic <xnetba@gmail.com>
2025-08-02 13:59:01 +0000

    Firebase: Error (auth/configuration-not-found).

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAnAHY6pg-cHWCIkOipc_qKeZUYuYsuzQk",
   authDomain: "procurecrm.firebaseapp.com",
   projectId: "procurecrm",
-  storageBucket: "procurecrm.firebasestorage.app",
+  storageBucket: "procurecrm.appspot.com",
   messagingSenderId: "289531756755",
   appId: "1:289531756755:web:6eed801e73589d4fd11426",
 };
